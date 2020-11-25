0df4f01e300690c8338c7a326c7ee45d18bca3d7
kdervic1 <kdervic1@etf.unsa.ba>
2020-11-25 21:42:55 +0100

    safety commit

---
+++ spirala2rasporedi.js
@@ -0,0 +1,4 @@
+import { iscrtajRaspored } from './iscrtaj.js'
+
+let okvir=document.getElementById("okvir");
+iscrtajRaspored("okvir",["Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak"],8,21);
